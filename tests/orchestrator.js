f1c8d8ef8b5bc1b856072aff60ff2ce7b77deee4
Gibran Tavares <mac01gibran@gmail.com>
2025-02-19 21:53:11 -0300

    make `npm run test` more robust

---
+++ tests/orchestrator.js
@@ -0,0 +1,22 @@
+import retry from "async-retry";
+
+async function waitForAllServices() {
+  await waitForWEbServer();
+
+  async function waitForWEbServer() {
+    return retry(fetchStatusPage, {
+      retries: 50,
+      maxTimeout: 1000,
+    });
+
+    async function fetchStatusPage() {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      
+      if (response.ok) {
+        throw new Error("Web server is not ready yet");
+      }
+    }
+  }
+}
+
+export default { waitForAllServices };
