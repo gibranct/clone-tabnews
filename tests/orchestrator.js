f2ef90cd3f8194a27e457d3da527920a52ba37e8
Gibran Tavares <mac01gibran@gmail.com>
2025-02-22 13:53:13 -0300

    fix test

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -12,7 +12,7 @@ async function waitForAllServices() {
     async function fetchStatusPage() {
       const response = await fetch("http://localhost:3000/api/v1/status");
       
-      if (response.ok) {
+      if (!response.ok) {
         throw new Error("Web server is not ready yet");
       }
     }
